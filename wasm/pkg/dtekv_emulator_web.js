e5ca1df5f730d9fa210d06d4a012b9b78f1b97e5
Fritiof Rusck <fritiof@rusck.se>
2024-11-14 16:19:31 +0100

    delete

--- wasm/pkg/dtekv_emulator_web.js
+++
@@ -1,4 +0,0 @@
-import * as wasm from "./dtekv_emulator_web_bg.wasm";
-export * from "./dtekv_emulator_web_bg.js";
-import { __wbg_set_wasm } from "./dtekv_emulator_web_bg.js";
-__wbg_set_wasm(wasm);
