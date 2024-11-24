82a42338dd86ea237bb6b9530e6b992ffca94c28
Fritiof Rusck <fritiof@rusck.se>
2024-11-24 11:20:07 +0100

    feat: remove old code

--- src/pages/Emulator/views/Nav/index.tsx
+++ src/pages/Emulator/views/Nav/index.tsx
@@ -1,16 +1,12 @@
-import { useRef, useState } from "react";
 import cx from "../../../../utils/cx";
 
 import styles from "./Nav.module.css";
 import {
   softReset,
   loadBinary,
-  loadDataAt,
-  readDataAt,
   reload,
 } from "../../../../cpu";
 import { GITHUB_URL } from "../../../../consts";
-import useOnClickOutside from "../../../../hooks/useOnClickOutside";
 import NavDropDownButton from "./helpers/NavDropDownButton";
 import { useAtomValue } from "jotai";
 import { hasLoadedAtom } from "../../../../atoms";
@@ -123,7 +119,7 @@ function AdvancedButton() {
 
   return (
     <NavDropDownButton
-      title="Advanced features"
+      title="Advanced"
       buttons={[
         {
           title: "Soft reset",
@@ -146,129 +142,6 @@ function AdvancedButton() {
       ]}
     />
   );
-}
-
-function DownloadButton() {
-  const contentRef = useRef<HTMLDivElement>(null);
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  const fileNameInput = useRef<HTMLInputElement>(null);
-  const addressInput = useRef<HTMLInputElement>(null);
-  const lengthInput = useRef<HTMLInputElement>(null);
-  const [active, setActive] = useState(false);
-  const [fileName, setFileName] = useState("data.bin");
-  const [address, setAddress] = useState("0x00000000");
-  const [length, setLength] = useState(1024);
-
-  useOnClickOutside([contentRef, buttonRef], () => setActive(false));
-
-  return (
-    <div className={styles.wrapper}>
-      <button
-        ref={buttonRef}
-        onClick={() => {
-          setActive(!active);
-        }}
-        className={styles.navButton}
-      >
-        Download
-      </button>
-      <div
-        ref={contentRef}
-        className={cx(styles.popoutContainer, active && styles.active)}
-      >
-        <div>
-          <label htmlFor="dtekv-download-filename" className={styles.formLabel}>
-            Filename
-          </label>
-          <input
-            ref={fileNameInput}
-            id="dtekv-download-filename"
-            className={styles.formInput}
-            value={fileName}
-            onInput={(e) => setFileName((e.target as HTMLInputElement).value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="dtekv-download-addr" className={styles.formLabel}>
-            Address
-          </label>
-          <input
-            ref={addressInput}
-            id="dtekv-download-addr"
-            className={styles.formInput}
-            value={address}
-            onInput={(e) => setAddress((e.target as HTMLInputElement).value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="dtekv-download-length" className={styles.formLabel}>
-            Length
-          </label>
-          <input
-            ref={lengthInput}
-            id="dtekv-download-length"
-            className={styles.formInput}
-            type="number"
-            min={0}
-            required
-            value={length || ""}
-            onInput={(e) =>
-              setLength(parseInt((e.target as HTMLInputElement).value))
-            }
-          />
-        </div>
-        <div>
-          <button
-            className={styles.formButton}
-            onClick={async () => {
-              const addr = parseAddress(address);
-              if (!fileName) {
-                showError(fileNameInput, "Please provide a file name.");
-                return;
-              }
-              if (addr === null) {
-                showError(addressInput, "Please provide an address.");
-                return;
-              }
-              if (isNaN(length) || length <= 0) {
-                showError(
-                  lengthInput,
-                  "Please provide an amount of bytes to download.",
-                );
-                return;
-              }
-
-              setActive(false);
-              let data: Uint8Array;
-              try {
-                data = readDataAt(addr, length);
-              } catch (e) {
-                alert(e);
-                return;
-              }
-              const blob = new Blob([data]);
-              downloadBlob(blob, fileName);
-            }}
-          >
-            Download
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function downloadBlob(blob: Blob, fileName: string) {
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = fileName;
-  a.style.display = "none";
-  document.body.appendChild(a);
-  a.click();
-  URL.revokeObjectURL(url);
 }
 
 export default Nav;
